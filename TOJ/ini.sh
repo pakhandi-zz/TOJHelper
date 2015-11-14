b7af6c5f65e001201da5636dcd7693748e75ef01
henadaus <hnfrds11@gmail.com>
2015-11-15 04:42:52 +0530

    Code_compiled

---
+++ TOJ/ini.sh
@@ -0,0 +1,4 @@
+DIR="$( cd "$( dirname "$0")" && pwd )"
+cd $DIR
+echo $DIR
+gnome-terminal -e "sh start.sh" 
---
+++ TOJ/zx.sh
@@ -0,0 +1,3 @@
+alias xdg-open="xdg-open 2>/dev/null"
+read qno
+xdg-open xdg-open http://acm.timus.ru/problem.aspx?num=$qno
